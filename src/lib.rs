36e610c26097a99393abb14a78f2f9581fc008c9
antoninhrlt <antoninhrlt1@gmail.com>
2022-05-09 22:51:55 +0200

    Organization update to fix mod access

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,6 @@
 // Under the MIT License
 // Copyright (c) Junon, Antonin Hérault
 
-#[path = "../crates/checking/src/lib.rs"]
 pub mod checking;
-
-#[path = "../crates/lang/src/lib.rs"]
 pub mod lang;
-
 pub mod parser;
