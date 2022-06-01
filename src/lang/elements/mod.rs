89d9255597434d997c2976f2a96584bd058b4de4
antoninhrlt <antoninhrlt1@gmail.com>
2022-06-01 17:22:58 +0200

    Create 'Element' enum for elements

--- src/lang/elements/mod.rs
+++ src/lang/elements/mod.rs
@@ -6,3 +6,16 @@ pub mod function;
 pub mod variable;
 pub mod params;
 pub mod type_;
+
+use function::Function;
+use variable::Variable;
+use params::Params;
+use type_::Type;
+
+/// Language's element that parsed will create from the tokens
+pub enum Element {
+    Function(Function),
+    Variable(Variable),
+    Params(Params),
+    Type(Type),
+}
